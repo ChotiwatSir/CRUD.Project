27da4a7b32b95e3cf8875ed2d92d2e6310a8e6d1
Hiyashiman <korawi.bun@gmail.com>
2024-07-16 17:35:47 +0700

    add homecontroller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Project.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class HomeController : ControllerBase
+    {
+
+
+    }
+}
